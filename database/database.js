66c65c83964a44e18cfd0486c8f0b8455ea1509d
jorgensta <jorgensta@gmail.com>
2017-09-15 11:21:24 +0200

    Laget Count variabel for arbeidsID
    Og gjort slik at databasen IKKE lager nytt dokument ved oppstart

--- database/database.js
+++ database/database.js
@@ -1,18 +1,18 @@
 let Datastore = require('nedb'); //henter databasen
 
-//Lager en database for alle arbeidere som er lagret i /filer/arbeidere.db, denne lager ny fil hver gang
+//Lager en database for alle arbeidere som er lagret i /filer/arbeidere.db, denne IKKE ny fil hver gang
 let arbeidere = new Datastore( {filename: __dirname + "/filer/arbeidere.db", autoload: true});
 
 // Lag en variabel = funksjon () som teller over antall objekt (Susanne)
 
-//Kunne laste inn databasen for å tak i data som er lagret og lagre ny data uten at vi oppretter ny database hver gang
+let count = arbeidere.count({}, function (err, count){
+  console.log("Count variable is now: ", count);
 
-
-//prøveprosjekt på å kunne laste inn database uten å opprette ny
-let arbeidere = arbeidere.loadDatabase(function(err){
-  console.log("fikk tak i arbeidere");
 });
 
+console.log(count);
+
+//Kunne laste inn databasen for å tak i data som er lagret og lagre ny data uten at vi oppretter ny database hver gang, dette er nå løst.
 
 
 let scott = {
@@ -22,14 +22,19 @@ let scott = {
 };
 
 
+/*
+
 //objectfactory for å lage nye brukere (ULRIK)
 const createUser ({navn, rolle});
+
+*/
 
 /*
 arbeidere.insert(scott, function(err,doc){
   console.log("Inserted", doc.navn, "arbeiderID: " , doc._id);
 });
 */
+
 
 
 arbeidere.findOne({id: '1'}, function(err,doc){
