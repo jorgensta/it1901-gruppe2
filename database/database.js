a3dbbc09163d7f2f132756f18cf9b516b6bf272a
jorgensta <jorgensta@gmail.com>
2017-09-20 20:32:37 +0200

    Laget unik ArbID og lekt meg litt

--- database/database.js
+++ database/database.js
@@ -5,21 +5,47 @@ let arbeidere = new Datastore( {filename: __dirname + "/filer/arbeidere.db", aut
 
 // Lag en variabel = funksjon () som teller over antall objekt (Susanne)
 
-let count = arbeidere.count({}, function (err, count){
-  console.log("Count variable is now: ", count);
+//Kunne laste inn databasen for å tak i data som er lagret og lagre ny data uten at vi oppretter ny database hver gang, dette er nå løst.
+
+let arbID = null;
+
+
+// Får en sortert liste og finner høyeste id, for så å sette den globale variabelen arbID = nowNumber ( som er høyeste ID)
+
+
+arbeidere.find({}).sort({id: 1}).exec(function (err,docs){
+  let nowNumber = 0;
+  docs.forEach(function(d){
+    if(d.id > nowNumber){
+      nowNumber = d.id;
+    };
+    console.log(d.navn);
+  });
+  console.log(nowNumber);
+  arbID = nowNumber;
+  console.log(arbID);
 
 });
 
-console.log(count);
+function incrementId(){
+  arbID = parseInt(arbID) + 1;
+}
 
-//Kunne laste inn databasen for å tak i data som er lagret og lagre ny data uten at vi oppretter ny database hver gang, dette er nå løst.
 
+function getID(){
+  setTimeout(function(){
+    incrementId();
+    incrementId();
+    console.log(arbID);
+  }, 1000);
 
-let scott = {
-  navn: 'Scott',
-  rolle: 'Manager',
-  id: '1'
 };
+
+getID();
+
+
+
+
 
 
 /*
@@ -28,14 +54,6 @@ let scott = {
 const createUser ({navn, rolle});
 
 */
-
-/*
-arbeidere.insert(scott, function(err,doc){
-  console.log("Inserted", doc.navn, "arbeiderID: " , doc._id);
-});
-*/
-
-
 
 arbeidere.findOne({id: '1'}, function(err,doc){
   console.log("Found user by ID = 1, name: " , doc.navn);
