7007d464f02d43d273ba27f297f8b1bce486e4b5
jorgtho <jorgen.thorsnes@gmail.com>
2017-10-09 11:32:35 +0200

    concert schema

---
+++ app/models/concert.js
@@ -0,0 +1,21 @@
+
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let Concert = new Schema({
+	concert: {
+		artist : String,
+    	scene : String,
+    	date : String,
+    	time : String,
+    	lights : String,
+    	sound : String,
+    	rig : String
+	},
+
+},{
+	collection: 'concerts'
+});
+
+
+module.exports = mongoose.model('Concert', Concert);
